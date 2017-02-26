360ce06e9fd9a3748341ad7803ee5a8e226bbd44
7LPdWcaW <7LPdWcaW@microgrowery>
2017-02-26 16:41:22 +0000

    Reverted to plain edit text

--- app/src/main/java/me/anon/growjournal/fragment/PostEditorFragment.java
+++ app/src/main/java/me/anon/growjournal/fragment/PostEditorFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Spannable;
 import android.text.SpannableString;
-import android.text.TextUtils;
 import android.text.style.DynamicDrawableSpan;
 import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
@@ -16,11 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import com.commonsware.cwac.richedit.Effect;
-import com.commonsware.cwac.richedit.RichEditText;
 import com.commonsware.cwac.richtextutils.SpannedXhtmlGenerator;
-
-import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -66,13 +61,13 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 
 	private View headingSizes;
 	private EditText title;
-	private RichEditText editor;
+	private EditText editor;
 
 	@Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.post_editor_view, container, false);
 		title = (EditText)view.findViewById(R.id.title);
-		editor = (RichEditText)view.findViewById(R.id.editor);
+		editor = (EditText)view.findViewById(R.id.editor);
 		headingSizes = view.findViewById(R.id.heading_sizes);
 		back = (ImageButton)view.findViewById(R.id.finish);
 		save = (ImageButton)view.findViewById(done);
@@ -89,7 +84,7 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 		heading3 = (Button)view.findViewById(R.id.heading_3);
 		heading4 = (Button)view.findViewById(R.id.heading_4);
 		formatBullet = (ImageButton)view.findViewById(R.id.bullet_list);
-//		formatNumber = (ImageButton)view.findViewById(R.id.number_list);
+		formatNumber = (ImageButton)view.findViewById(R.id.number_list);
 
 		back.setOnClickListener(this);
 		save.setOnClickListener(this);
@@ -106,7 +101,7 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 		heading2.setOnClickListener(this);
 		heading3.setOnClickListener(this);
 		heading4.setOnClickListener(this);
-//		formatNumber.setOnClickListener(this);
+		formatNumber.setOnClickListener(this);
 
 		return view;
 	}
@@ -114,45 +109,6 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 	@Override public void onActivityCreated(@Nullable Bundle savedInstanceState)
 	{
 		super.onActivityCreated(savedInstanceState);
-
-		editor.setOnSelectionChangedListener(new RichEditText.OnSelectionChangedListener()
-		{
-			@Override public void onSelectionChanged(int start, int end, List<Effect<?>> effects)
-			{
-				toggleButtonsOff();
-
-				if (effects.size() > 0)
-				{
-					for (Effect<?> effect : effects)
-					{
-						if (effect == RichEditText.BOLD)
-						{
-							formatBold.setSelected(true);
-						}
-						else if (effect == RichEditText.ITALIC)
-						{
-							formatItalic.setSelected(true);
-						}
-						else if (effect == RichEditText.UNDERLINE)
-						{
-							formatUnderline.setSelected(true);
-						}
-						else if (effect == RichEditText.URL)
-						{
-							formatLink.setSelected(true);
-						}
-						else if (effect == RichEditText.BULLET)
-						{
-							formatBullet.setSelected(true);
-						}
-//						else if (effect == RichEditText.NUMBER_LIST)
-//						{
-//							formatNumber.setSelected(true);
-//						}
-					}
-				}
-			}
-		});
 	}
 
 	private void toggleButtonsOff()
@@ -165,7 +121,7 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 		formatAttach.setSelected(false);
 		formatHeading.setSelected(false);
 		formatBullet.setSelected(false);
-//		formatNumber.setSelected(false);
+		formatNumber.setSelected(false);
 	}
 
 	@Override public void onClick(View v)
@@ -184,52 +140,31 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 		}
 		else if (v == formatBold)
 		{
-			if (editor.hasEffect(RichEditText.ABSOLUTE_SIZE))
-			{
-				v.setSelected(true);
-				return;
-			}
 
-			editor.toggleEffect(RichEditText.BOLD);
 		}
 		else if (v == formatItalic)
 		{
-			editor.toggleEffect(RichEditText.ITALIC);
+
 		}
 		else if (v == formatUnderline)
 		{
-			editor.toggleEffect(RichEditText.UNDERLINE);
+
 		}
 		else if (v == formatLink)
 		{
-			boolean isLink = editor.hasEffect(RichEditText.URL);
-			String url = editor.getEffectValue(RichEditText.URL);
-
-			EditLinkDialogFragment dialogFragment = new EditLinkDialogFragment(url);
+			EditLinkDialogFragment dialogFragment = new EditLinkDialogFragment(null);
 			dialogFragment.setOnEditLinkListener(new EditLinkDialogFragment.OnEditLinkListener()
 			{
 				@Override public void onLinkEdited(String text, String url)
 				{
-					if (!TextUtils.isEmpty(text))
-					{
-						editor.getText().insert(editor.getSelectionStart(), text);
-						editor.setSelection(editor.getSelectionStart(), text.length());
-					}
 
-					editor.applyEffect(RichEditText.URL, url);
 				}
 			});
 			dialogFragment.show(getChildFragmentManager(), null);
 		}
 		else if (v == formatBullet)
 		{
-//			if (editor.hasEffect(RichEditText.NUMBER_LIST))
-//			{
-//				editor.toggleEffect(RichEditText.NUMBER_LIST);
-//				formatNumber.setSelected(false);
-//			}
 
-			editor.toggleEffect(RichEditText.BULLET);
 		}
 		else if (v == formatHeading)
 		{
@@ -238,52 +173,29 @@ public class PostEditorFragment extends Fragment implements View.OnClickListener
 		}
 		else if (v == heading1)
 		{
-			heading2.setSelected(false);
-			heading3.setSelected(false);
-			heading4.setSelected(false);
 
-			editor.applyEffect(RichEditText.ABSOLUTE_SIZE, 42);
 		}
 		else if (v == heading2)
 		{
-			heading1.setSelected(false);
-			heading3.setSelected(false);
-			heading4.setSelected(false);
 
-			editor.applyEffect(RichEditText.ABSOLUTE_SIZE, 36);
 		}
 		else if (v == heading3)
 		{
-			heading1.setSelected(false);
-			heading2.setSelected(false);
-			heading4.setSelected(false);
 
-			editor.applyEffect(RichEditText.ABSOLUTE_SIZE, 32);
 		}
 		else if (v == heading4)
 		{
-			heading1.setSelected(false);
-			heading2.setSelected(false);
-			heading3.setSelected(false);
 
-			editor.applyEffect(RichEditText.ABSOLUTE_SIZE, 28);
 		}
 		else if (v == save)
 		{
 			savePost();
 			getActivity().finish();
 		}
+		else if (v == formatNumber)
+		{
 
-//		else if (v == formatNumber)
-//		{
-//			if (editor.hasEffect(RichEditText.BULLET))
-//			{
-//				editor.toggleEffect(RichEditText.BULLET);
-//				formatBullet.setSelected(false);
-//			}
-//
-//			editor.toggleEffect(RichEditText.NUMBER_LIST);
-//		}
+		}
 	}
 
 	private void savePost()
