131cb3d0e3a5ed42d7edffbdbfa6c028cb0bbea5
7LPdWcaW <7LPdWcaW@microgrowery>
2017-02-26 16:38:22 +0000

    Added main application

---
+++ app/src/main/java/me/anon/growjournal/MainApplication.java
@@ -0,0 +1,25 @@
+package me.anon.growjournal;
+
+import android.app.Application;
+
+import java.io.File;
+
+import me.anon.growjournal.manager.PostsManager;
+
+/**
+ * // TODO: Add class description
+ */
+public class MainApplication extends Application
+{
+	@Override public void onCreate()
+	{
+		super.onCreate();
+
+		PostsManager.folderPath = getFilesDir().getAbsolutePath() + "/posts/";
+
+		if (!new File(PostsManager.folderPath).exists())
+		{
+			new File(PostsManager.folderPath).mkdirs();
+		}
+	}
+}
