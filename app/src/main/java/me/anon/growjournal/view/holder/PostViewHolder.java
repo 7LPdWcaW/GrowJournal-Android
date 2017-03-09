2dc06b3c5bbdd8a3eab2ed83883b6be8e3857c31
7LPdWcaW <7LPdWcaW@microgrowery>
2017-03-09 16:10:10 +0000

    Added popup window position based on click position

--- app/src/main/java/me/anon/growjournal/view/holder/PostViewHolder.java
+++ app/src/main/java/me/anon/growjournal/view/holder/PostViewHolder.java
@@ -1,10 +1,15 @@
 package me.anon.growjournal.view.holder;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -27,6 +32,9 @@ public class PostViewHolder extends RecyclerView.ViewHolder
 	private TextView summary;
 	private TextView date;
 	private View publishStatus;
+
+	// temp variables for touch location
+	private transient int x, y;
 
 	public PostViewHolder(View itemView)
 	{
@@ -61,11 +69,44 @@ public class PostViewHolder extends RecyclerView.ViewHolder
 			}
 		});
 
+		itemView.setOnTouchListener(new View.OnTouchListener()
+		{
+			@Override public boolean onTouch(View v, MotionEvent event)
+			{
+				final int action = event.getAction();
+				switch (action & MotionEvent.ACTION_MASK)
+				{
+					case MotionEvent.ACTION_DOWN:
+					{
+						x = (int)event.getX();
+						y = (int)event.getY();
+						break;
+					}
+				}
+
+				return false;
+			}
+		});
+
 		itemView.setOnLongClickListener(new View.OnLongClickListener()
 		{
 			@Override public boolean onLongClick(final View v)
 			{
-				PopupMenu popup = new PopupMenu(v.getContext(), v);
+				ViewGroup root = (ViewGroup)((Activity)v.getContext()).getWindow().getDecorView().findViewById(android.R.id.content);
+
+				View view = new View(v.getContext());
+				view.setLayoutParams(new ViewGroup.LayoutParams(1, 1));
+				view.setBackgroundColor(Color.TRANSPARENT);
+
+				root.addView(view);
+
+				int[] viewPos = new int[2];
+				v.getLocationInWindow(viewPos);
+
+				view.setX(x);
+				view.setY(y + viewPos[1]);
+
+				PopupMenu popup = new PopupMenu(v.getContext(), view, Gravity.CENTER);
 
 				popup.getMenuInflater().inflate(R.menu.post_menu, popup.getMenu());
 				popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
