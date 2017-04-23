4917ea1e816006647199f8136a150ffec180f212
7LPdWcaW <7LPdWcaW@microgrowery>
2017-04-23 20:47:42 +0100

    Added base site settings config

--- app/src/main/java/me/anon/growjournal/activity/SettingsActivity.java
+++ app/src/main/java/me/anon/growjournal/activity/SettingsActivity.java
@@ -1,12 +1,23 @@
 package me.anon.growjournal.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.util.Map;
 
 import me.anon.growjournal.R;
+import me.anon.growjournal.manager.GitManager;
 
 /**
  * // TODO: Add class description
@@ -28,11 +39,95 @@ public class SettingsActivity extends AppCompatActivity
 
 	public static class SettingsFragment extends PreferenceFragment
 	{
+		private Map siteSettings;
+    	private SharedPreferences.OnSharedPreferenceChangeListener listener;
+
 		@Override public void onActivityCreated(@Nullable Bundle savedInstanceState)
 		{
 			super.onActivityCreated(savedInstanceState);
 
 			addPreferencesFromResource(R.xml.settings);
+
+			loadSettings();
+			populateSummaries();
+
+			createListener();
+		}
+
+		@Override public void onDestroy()
+		{
+			super.onDestroy();
+
+			saveSettings();
+			GitManager.getInstance().commitChanges();
+		}
+
+		private void populateSummaries()
+		{
+			findPreference("site_title").setSummary((String)siteSettings.get("title"));
+			findPreference("site_description").setSummary((String)siteSettings.get("description"));
+			findPreference("site_base_url").setSummary((String)siteSettings.get("baseurl"));
+			findPreference("site_url").setSummary((String)siteSettings.get("url"));
+		}
+
+		private void loadSettings()
+		{
+			try
+			{
+				Yaml yaml = new Yaml();
+				siteSettings = (Map)yaml.load(new FileInputStream(new File(GitManager.getInstance().getLocalRepo(), "_config.yml" )));
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				Toast.makeText(getActivity(), "Failed to load settings, ensure your _config.yml is correct", Toast.LENGTH_SHORT).show();
+				getActivity().finish();
+			}
+		}
+
+		private void saveSettings()
+		{
+			try
+			{
+				Yaml yaml = new Yaml();
+				yaml.dump(siteSettings, new FileWriter(new File(GitManager.getInstance().getLocalRepo(), "_config.yml" )));
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				Toast.makeText(getActivity(), "Failed to save settings, ensure your _config.yml is correct", Toast.LENGTH_SHORT).show();
+				getActivity().finish();
+			}
+		}
+
+		private void createListener()
+		{
+			listener = new SharedPreferences.OnSharedPreferenceChangeListener()
+			{
+				@Override public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+				{
+					populateSummaries();
+
+					if (key.equals("site_title"))
+					{
+						siteSettings.put("title", sharedPreferences.getString("site_title", ""));
+					}
+					else if (key.equals("site_description"))
+					{
+						siteSettings.put("description", sharedPreferences.getString("description", ""));
+					}
+					else if (key.equals("site_base_url"))
+					{
+						siteSettings.put("baseurl", sharedPreferences.getString("site_base_url", ""));
+					}
+					else if (key.equals("site_url"))
+					{
+						siteSettings.put("url", sharedPreferences.getString("site_url", ""));
+					}
+				}
+			};
+
+			PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(listener);
 		}
 	}
 }
