ec5c5e87b934ee633555dda8a2f6de43426d287d
7LPdWcaW <7LPdWcaW@microgrowery>
2017-04-23 21:01:15 +0100

    Updated default post and added config parsing

--- app/src/main/java/me/anon/growjournal/model/Post.java
+++ app/src/main/java/me/anon/growjournal/model/Post.java
@@ -2,7 +2,13 @@ package me.anon.growjournal.model;
 
 import android.support.annotation.Nullable;
 
+import org.yaml.snakeyaml.Yaml;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 import lombok.Getter;
@@ -17,7 +23,7 @@ public class Post
 	@Getter @Setter private String id = UUID.randomUUID().toString();
 	@Getter @Setter private String title;
 	@Getter @Setter private String body;
-	@Getter @Setter private long createdDate;
+	@Getter @Setter private ArrayList<String> categories = new ArrayList<>();
 	@Getter @Setter private long publishDate;
 	@Getter @Setter private long updateDate;
 	@Getter @Setter private PublishStatus publishStatus;
@@ -36,8 +42,19 @@ public class Post
 			String postBody = FileManager.getInstance().readFileAsString(filePath);
 
 			Post post = new Post();
-			post.setBody(postBody);
-			post.setTitle(new File(filePath).getName());
+
+			int start = postBody.indexOf("---", 0);
+			int end = postBody.indexOf("---", start + 3);
+			String configBlock = postBody.substring(start, end);
+			Map configYaml = (Map)new Yaml().load(configBlock);
+
+			post.setTitle((String)configYaml.get("title"));
+
+			String[] categories = ((String)configYaml.get("categories")).split(" ");
+			post.setCategories(new ArrayList<String>(Arrays.asList(categories)));
+
+			post.setBody(postBody.substring(end + 3));
+			post.setPublishDate(((Date)configYaml.get("date")).getTime());
 			post.setUpdateDate(new File(filePath).lastModified());
 
 			return post;
