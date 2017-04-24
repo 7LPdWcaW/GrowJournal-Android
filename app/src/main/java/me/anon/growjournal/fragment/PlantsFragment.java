5e1b92905d0c805b2231879cb63ecb457d9285db
7LPdWcaW <7LPdWcaW@microgrowery>
2017-04-24 15:10:44 +0100

    Added basic plant fragment functionality

--- app/src/main/java/me/anon/growjournal/fragment/PlantsFragment.java
+++ app/src/main/java/me/anon/growjournal/fragment/PlantsFragment.java
@@ -3,9 +3,14 @@ package me.anon.growjournal.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import me.anon.growjournal.R;
+import me.anon.growjournal.adapter.PlantsAdapter;
 
 /**
  * // TODO: Add class description
@@ -17,8 +22,31 @@ public class PlantsFragment extends Fragment
 		return new PlantsFragment();
 	}
 
+	private RecyclerView recyclerView;
+	private PlantsAdapter adapter;
+
 	@Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
-		return super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.plants_view, container, false);
+		recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view);
+
+		return view;
+	}
+
+	@Override public void onActivityCreated(@Nullable Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+
+		if (savedInstanceState == null)
+		{
+			initialiseAdapter();
+		}
+	}
+
+	private void initialiseAdapter()
+	{
+		adapter = new PlantsAdapter();
+		recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+		recyclerView.setAdapter(adapter);
 	}
 }
