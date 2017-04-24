3a2e341bb07ef1d794743b1d6f4b212d17880389
7LPdWcaW <7LPdWcaW@microgrowery>
2017-04-24 15:11:04 +0100

    Re-load plants into models when receiving plant updates

--- app/src/main/java/me/anon/growjournal/receiver/GrowTrackerReceiver.java
+++ app/src/main/java/me/anon/growjournal/receiver/GrowTrackerReceiver.java
@@ -23,6 +23,7 @@ public class GrowTrackerReceiver extends BroadcastReceiver
 
 			new File(GitManager.getInstance().getLocalRepo().getAbsolutePath() + "/_data/").mkdirs();
 			FileManager.getInstance().writeFile(GitManager.getInstance().getLocalRepo().getAbsolutePath() + "/_data/plants.json", plantData);
+			PlantManager.getInstance().load();
 			PlantManager.getInstance().regeneratePages();
 			GitManager.getInstance().commitChanges();
 		}
